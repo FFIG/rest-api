87d4ab75388ef8a12c6f3db04c03ee9d60bf557d
petr-tik <petr-tik@users.noreply.github.com>
2018-04-13 00:04:35 +0100

    Re-writing the API
    Idea: work out the number of lines of code that the ffig-explorer
    web-app needs to turn an input file string into a binding that can be
    posted back to the client

--- ffig_explorer.py
+++ ffig_explorer.py
@@ -6,35 +6,12 @@ import pdb
 import json
 import shutil
 
+
 """ Flask app that implements a basic front-end on the home page and a rest-api under the /api/ endpoints """
 
 app = Flask(__name__, template_folder=".")
 
 CUR_DIR = os.path.dirname(__file__)
-
-
-class Args:
-    """ Class, which creates a duckly-typed Object
-    with same attributes as normal FFIG argparser"""
-
-    def __init__(self, path_to_source, module_name,
-                 bindings, output_dir='output', template_dir=None):
-
-        self.inputs = [os.path.abspath(path_to_source)]
-        # FFIG.py takes an array of inputs (for future),
-        # but currently only uses the 0th
-        self.module_name = module_name
-        self.bindings = list(bindings)
-        if template_dir == None:
-            ffig_subfolder = '/home/ffig/ffig/'
-            self.template_dir = os.path.join(ffig_subfolder, 'templates')
-        self.output_dir = os.path.join(CUR_DIR, output_dir)
-
-    def __repr__(self):
-        obj_str = "Module_name: {}, bindings: {}, \
-                   template_dir: {}, inputs: {}".format(
-            self.module_name, self.bindings, self.template_dir, self.inputs)
-        return obj_str
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -43,7 +20,6 @@ def home():
 
 
 @app.route('/api/gen_bindings_from_tu', methods=['POST'])
-#@app.after_request
 def gen_bindings_from_tu():
     """ Route method that receives a POST request and 
     returns a JSON response with requested bindings in plaintext 
@@ -53,28 +29,29 @@ def gen_bindings_from_tu():
     mod_name = request.form['module_name']
     inp_file = request.form['inp_file']
 
-    path_to_source = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), 'headerfile'))
-    args = Args(path_to_source, mod_name, ["python"])
-    with open(path_to_source, 'w') as f:
-        f.write(inp_file)
-    # pdb.set_trace()
-    FFIG.main(args)
+    # local re-write/copy of ffig functionality
+    ffig_subfolder = '/home/ffig/ffig/'
+    template_dir = os.path.join(ffig_subfolder, 'templates')
 
-    py2_bind_name = os.path.join(args.output_dir, mod_name, "interop_py2.py")
-    py3_bind_name = os.path.join(args.output_dir, mod_name, "interop_py3.py")
-    with app.open_resource(py2_bind_name, 'rb') as py2_bd, \
-            app.open_resource(py3_bind_name, 'rb') as py3_bd:
-        return jsonify(status=200, module_name=mod_name,
-                       inp_file=inp_file, py3_bind=py3_bd.read(),
-                       py2_bind=py2_bd.read())
+    env = ffig.FFIG.set_template_env(template_dir)
+
+    template = env.get_template("py3.tmpl")
+
+    m = ffig.FFIG.build_model_from_source(inp_file, mod_name)
+    classes = m.classes
+    api_classes = ffig.FFIG.collect_api_and_obj_classes(classes, 'FFIG:EXPORT')
+    output_string = ffig.generators.render_api_and_obj_classes(
+        mod_name, api_classes, template)
+
+    return jsonify(status=200,
+                   module_name=mod_name,
+                   inp_file=inp_file)
 
 
 if __name__ == '__main__':
     if os.path.exists('/.dockerenv'):
         # check that we are inside a container
-        # FIXME runs twice
-        import FFIG
-        app.run(host='0.0.0.0', port=5000, debug=False)
+        import ffig.FFIG
+        app.run(host='0.0.0.0', port=5000, debug=True)
     else:
         raise Exception("Build and run a container")
