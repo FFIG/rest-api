2a94bf789a04f0e10eabd878b97697b5b9124eab
petr-tik <petr-tik@users.noreply.github.com>
2018-04-25 23:36:32 +0100

    Move travis install step into a separate shell script

---
+++ prepare_deps.sh
@@ -0,0 +1,13 @@
+pip3 install --upgrade pip && pip3 install flask
+
+ls -al ../
+
+git clone -b master --recurse-submodules https://github.com/FFIG/ffig.git ../ffig
+
+touch ../ffig/__init__.py ../ffig/ffig/templates/__init__.py
+
+ls -al ../
+
+cd -
+
+export PYTHONPATH="${PYTHONPATH}:../ffig/"
