6620e2903678ce888acf4d610e78cc14fbfaaf89
petr-tik <petr-tik@users.noreply.github.com>
2018-04-26 00:23:39 +0100

    Remove dead ls and add another init

--- prepare_deps.sh
+++ prepare_deps.sh
@@ -1,13 +1,8 @@
 pip3 install --upgrade pip && pip3 install flask
 
-ls -al ../
-
 git clone -b master --recurse-submodules https://github.com/FFIG/ffig.git ../ffig
 
-touch ../ffig/__init__.py ../ffig/ffig/templates/__init__.py
-
-ls -al ../ffig
+touch ../ffig/__init__.py ../ffig/ffig/__init__.py ../ffig/ffig/templates/__init__.py
 
 export PYTHONPATH="${PYTHONPATH}:../ffig/:../ffig/ffig/"
-env | grep -i python
 
