c81355bb7d75286d17ade2e14b73217f69d89691
petr-tik <petr-tik@users.noreply.github.com>
2018-04-26 00:42:03 +0100

    Reduce the import search space

--- prepare_deps.sh
+++ prepare_deps.sh
@@ -2,7 +2,7 @@ pip3 install --upgrade pip && pip3 install flask
 
 git clone -b master --recurse-submodules https://github.com/FFIG/ffig.git ../ffig
 
-touch ../ffig/__init__.py ../ffig/ffig/__init__.py ../ffig/ffig/templates/__init__.py
+touch ../ffig/__init__.py ../ffig/ffig/templates/__init__.py
 
-export PYTHONPATH="${PYTHONPATH}:../ffig/:../ffig/ffig/"
+export PYTHONPATH="${PYTHONPATH}:../ffig/"
 
