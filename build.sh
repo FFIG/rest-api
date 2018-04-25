12c60a553dfedd9b780f852d20cb41a35ef9415e
petr-tik <petr-tik@users.noreply.github.com>
2018-04-25 14:57:34 +0100

    Remove pip2
    fix dir paths for ffig main dependency

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
-pip2 install --upgrade pip && \
-    pip2 install flask && \
-    pip3 install --upgrade pip && \
-    pip3 install flask
+pip3 install --upgrade pip && pip3 install flask
 
-mdkir -p home
+cd ../ && git clone -b master --recurse-submodules https://github.com/FFIG/ffig.git
 
-cd /home && \
-    git clone -b master --recurse-submodules https://github.com/FFIG/ffig.git
-
-cd /home/ffig && git pull origin master && cd ../
+cd /ffig && git pull origin master && cd ../rest-api
 
 touch home/ffig/__init__.py home/ffig/ffig/templates/__init__.py
 
