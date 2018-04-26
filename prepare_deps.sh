1770b7a57e1ce055f07ac119a5f93d11a6fad868
petr-tik <petr-tik@users.noreply.github.com>
2018-04-26 01:06:46 +0100

    find and set ld_lib_path with libclang

--- prepare_deps.sh
+++ prepare_deps.sh
@@ -5,4 +5,4 @@ git clone -b master --recurse-submodules https://github.com/FFIG/ffig.git ../ffi
 touch ../ffig/__init__.py ../ffig/ffig/templates/__init__.py
 
 export PYTHONPATH="${PYTHONPATH}:../ffig/"
-
+export LD_LIBRARY_PATH=$(dirname $(find / -name libclang*.so))
