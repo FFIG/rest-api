1f338d9a51889cccd6f865a9e8c7fd72f8b4f71e
petr-tik <petr-tik@users.noreply.github.com>
2018-05-09 01:03:30 +0100

    Install libclang-50 and make prepare_deps executable

--- prepare_deps.sh
+++ prepare_deps.sh
@@ -5,4 +5,6 @@ git clone -b master --recurse-submodules https://github.com/FFIG/ffig.git ../ffi
 touch ../ffig/__init__.py ../ffig/ffig/templates/__init__.py
 
 export PYTHONPATH="${PYTHONPATH}:../ffig/"
-export LD_LIBRARY_PATH=$(llvm-config --libdir)
+
+apt-get install libclang-5.0-dev
+export LD_LIBRARY_PATH=/usr/lib/llvm-5.0/lib:$LD_LIBRARY_PATH
